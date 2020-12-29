4c3c0717aaf1151fe5c7c50776a040f8501be85d
A.J. Orians <ajorians@gmail.com>
2020-12-29 15:48:05 -0500

    Started writing a resource finder class.

--- LocalizationValidatorLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LocalizationValidatorLib
-{
-   public class Class1
-   {
-   }
-}
---
+++ LocalizationValidatorLib/ResourceFinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocalizationValidatorLib
+{
+   public class ResourceFinder
+   {
+      private readonly string _directory;
+      public ResourceFinder( string directory )
+      {
+         _directory = directory;
+      }
+
+      public IEnumerable<string> GetAllResourceFiles()
+      {
+         yield break;
+      }
+   }
+}
