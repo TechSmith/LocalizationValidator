9e938327383820a69acd7c6f9f5bbbbb84db543d
A.J. Orians <ajorians@gmail.com>
2020-12-29 16:54:36 -0500

    Added a manager class to manage finding and validating resource files.

--- LocalizationValidator/Program.cs
+++ LocalizationValidator/Program.cs
@@ -7,18 +7,11 @@ namespace LocalizationValidator
 {
    class Program
    {
-      static void Main( string[] args )
+      static int Main( string[] args )
       {
-         ResourceExtensions resourceExtensions = new ResourceExtensions();
-         ResourceFinder resourceFinder = new ResourceFinder( args.First(), new DirectoryWrap(), new AllValidPaths() );
+         LocalizationValidatorManager manager = new LocalizationValidatorManager( args.First() );
          
-         foreach ( var resourceFile in resourceFinder.GetAllResourceFiles() )
-         {
-            if ( resourceExtensions.HasResxExtension( resourceFile ) )
-            {
-               ResxReader reader = new ResxReader( resourceFile, new FileWrap() );
-            }
-         }
+         return manager.ValidateResources();
       }
    }
 
---
+++ LocalizationValidatorLib/LocalizationValidatorManager.cs
@@ -0,0 +1,29 @@
+﻿using SystemWrapper.IO;
+
+namespace LocalizationValidatorLib
+{
+   public class LocalizationValidatorManager
+   {
+      private readonly string _directory;
+      public LocalizationValidatorManager( string directory )
+      {
+         _directory = directory;
+      }
+      
+      public int ValidateResources()
+      {
+         ResourceExtensions resourceExtensions = new ResourceExtensions();
+         ResourceFinder resourceFinder = new ResourceFinder( _directory, new DirectoryWrap(), new AllValidPaths() );
+         
+         foreach ( var resourceFile in resourceFinder.GetAllResourceFiles() )
+         {
+            if ( resourceExtensions.HasResxExtension( resourceFile ) )
+            {
+               ResxReader reader = new ResxReader( resourceFile, new FileWrap() );
+            }
+         }
+
+         return 0;
+      }
+   }
+}
